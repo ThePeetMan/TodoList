4c81bd4ef980e94440b1e5d0c691f1b84f457d8b
Chittaworn_Peet <Chittaworn.tp@gmail.com>
2024-07-18 18:31:00 +0700

    Update ToDoList

--- todolist-project/src/app/components/todolist.tsx
+++ todolist-project/src/app/components/todolist.tsx
@@ -1,44 +1,74 @@
 "use client";
-import { Table } from "antd";
+
+import React from "react";
+import {
+  Divider,
+  List,
+  Typography,
+  Input,
+  Space,
+  Button,
+  Flex,
+  Tooltip,
+} from "antd";
+import { SearchOutlined } from "@ant-design/icons";
+import { Yellowtail } from "next/font/google";
 
 export default function TodoList() {
-    const dataSource = [
-        {
-          key: '1',
-          name: 'Mike',
-          age: 32,
-          address: '10 Downing Street',
-        },
-        {
-          key: '2',
-          name: 'John',
-          age: 42,
-          address: '10 Downing Street',
-        },
-      ];
-      
-      const columns = [
-        {
-          title: 'Name',
-          dataIndex: 'name',
-          key: 'name',
-        },
-        {
-          title: 'Age',
-          dataIndex: 'age',
-          key: 'age',
-        },
-        {
-          title: 'Address',
-          dataIndex: 'address',
-          key: 'address',
-        },
-      ];
-      
-      <Table dataSource={dataSource} columns={columns} />;
+  const data = [
+    "Racing car sprays burning fuel into crowd.",
+    "Japanese princess to wed commoner.",
+    "Australian walks 100km after outback crash.",
+    "Man charged over missing wedding girl.",
+    "Los Angeles battles huge wildfires.",
+  ];
+
   return (
     <>
-      <Table dataSource={dataSource} columns={columns}></Table>
+      <div style={{ display: "flex" }}>
+        <Input
+          placeholder="To Do Some Thing"
+          style={{ margin: "10px", width: "100%" }}
+        />
+        <Button type="primary" style={{ margin: "10px" }}>
+          Add
+        </Button>
+        <Tooltip title="search">
+          <Button
+            type="primary"
+            shape="circle"
+            icon={<SearchOutlined />}
+            style={{ margin: "10px 10px 10px 0px" }}
+          />
+        </Tooltip>
+      </div>
+
+      <List
+        bordered
+        dataSource={data}
+        renderItem={(item) => (
+          <List.Item>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                width: "100%",
+              }}
+            >
+              {" "}
+              <div>
+                <Typography.Text mark>[ITEM]</Typography.Text> {item}
+              </div>
+              <Flex gap="small">
+                <Button style={{ background: "#FFBF00" }}>Edit</Button>
+                <Button type="primary" danger>
+                  Delete
+                </Button>
+              </Flex>
+            </div>
+          </List.Item>
+        )}
+      />
     </>
   );
 }
